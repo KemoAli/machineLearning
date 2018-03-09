f454a883058a29b7b9ead3063d4448dd0aa9936e
shokoors <shokoors@buffalo.edu>
2018-03-09 16:29:28 -0500

    added feed forward and sigmoid func

--- nnScript.py
+++ nnScript.py
@@ -28,7 +28,7 @@ def sigmoid(z):
     """# Notice that z can be a scalar, a vector or a matrix
     # return the sigmoid of input z"""
 
-    return  # your code here
+    return  1/(1+np.exp(-z))
 
 
 def preprocess():
@@ -196,7 +196,18 @@ def nnObjFunction(params, *args):
     #
     #
     #
+    
+    #Forward propogation
+    #adding biases to input data with the size of training_data columns 
+    training_data = np.column_stack((training_data,np.ones(training_data.shape[0])))
+    #Multiplying and making the function non linear
+    zj_hidden1  = sigmoid(np.dot(training_data, w1.T))
 
+    #adding bias to hidden layer1 with the size of zj_hidden1 columns
+    zj_hidden1 = np.column_stack((zj_hidden1,np.ones(zj_hidden1.shape[0])))
+    #Multiplying and making the function non linear
+    outputs = sigmoid(np.dot(zj_hidden1, w2.T))
+    
 
 
     # Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
